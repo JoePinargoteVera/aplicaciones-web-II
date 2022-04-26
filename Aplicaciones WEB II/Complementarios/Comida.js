b99abe436a7515196c7f580904816973a810bee4
joeManuel <104092785+JoePinargoteVera@users.noreply.github.com>
2022-04-25 19:43:41 -0500

    second

--- Aplicaciones WEB II/Complementarios/Comida.js
+++ Aplicaciones WEB II/Complementarios/Comida.js
@@ -6,14 +6,43 @@ class Comida {
         this.precio = precio;
     }
     getComida() {
-        return `${this.comidaNombre} ${this.tipo} ${this.ingredientes} ${this.precio}`;
+        return `el ${this.comidaNombre} es un ${this.tipo} que tiene como principales ingredientes: ${this.ingredientes} y cuyo precio es: ${this.precio}`;
     }
 }
 
 const ComidasFavoritas = [
-     Comida1 = new Comida("encebollado", ['cebolla', "yuca"], 'plato fuerte', 2,50),
-     Comida2 = new Comida("ceviche", ['pescado', "tomate"], 'plato fuerte', 2)
+     Comida1 = new Comida("encebollado", ['cebolla', "yuca"], 'plato fuerte', '2,50'),
+     Comida2 = new Comida("ceviche", ['pescado', "tomate"], 'plato fuerte', '2')
 ]
-ComidasFavoritas.forEach(eat => {
-    console.log(eat.getComida())
-});
+// ComidasFavoritas.forEach(comida => {
+//     console.log(comida.getComida())
+// });
+
+// let j =0;
+// do {
+//     let element = ComidasFavoritas[j];
+//     console.log(element.getComida())
+//     j++;
+// } while (j < ComidasFavoritas.length);
+
+// let i = 0;
+// while (i < ComidasFavoritas.length) {
+    
+//     let element = ComidasFavoritas[i];
+//     console.log(element.getComida())
+//     i++;
+// }
+
+// for (let i = 0; i < ComidasFavoritas.length; i++) {
+//     let element = ComidasFavoritas[i];
+//     console.log(element.getComida())
+    
+// }
+
+const Amayusculas = (comidas)=>{
+    comidas.forEach(comida => {
+        console.log(comida.toUpperCase())
+    });
+}
+
+Amayusculas(ComidasFavoritas)
---
+++ Aplicaciones WEB II/Ejercicios en clase/Async/callback.js
@@ -0,0 +1,70 @@
+const platos =[
+    {
+        id: 1,
+        descripcion: 'chaulafan',
+        idrestaurante:1
+    },
+    {
+        id: 2,
+        descripcion: 'arroz con camaron',
+        idrestaurante:1
+    },
+    {
+        id: 3,
+        descripcion: 'parrillada',
+        idrestaurante: 2
+    },
+    {
+        id: 4,
+        descripcion: 'chuzo',
+        idrestaurante: 2
+    }
+]
+
+const restaurantes =[
+    {
+        id:1,
+        nombre:'chifa manta'
+    },
+    {
+        id:2,
+        nombre:'la parrillada del loco'
+    }
+]
+
+function buscarPlatoPorId(id, callback)
+{
+    const plato = platos.find((plato)=>plato.id === id);
+    if (!plato) {
+        const error = new Error();
+        error.message = `el plato con id ${id} no pudo ser encontrado`;
+        return callback(error);
+    }
+    return callback(null, plato);
+}
+
+function buscarRestaurantePorId(id, callback)
+{
+    const restaurante = restaurantes.find((restaurante)=>restaurante.id === id);
+    if (!restaurante) {
+        const error = new Error();
+        error.message = `el restaurante con id ${id} no pudo ser encontrado`;
+        return callback(error);
+    }
+    return callback(null, restaurante);
+}
+// buscarPlatoPorId(123, (err, plato)=>{
+//     if (err) {
+//         console.log(err.message);
+//         return;
+//     }
+//     console.log(plato);
+// })
+
+buscarRestaurantePorId(3, (err, restaurante)=>{
+    if (err) {
+        console.log(err.message); 
+        return;       
+    }
+    console.log(restaurante);
+})
