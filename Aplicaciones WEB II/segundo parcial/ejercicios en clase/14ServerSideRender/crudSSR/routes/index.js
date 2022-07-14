d7706ff075f817a43233271340406c49e86d6a1f
joePinargoteVera <104092785+JoePinargoteVera@users.noreply.github.com>
2022-07-14 01:09:23 -0500

    start SSR

---
+++ Aplicaciones WEB II/segundo parcial/ejercicios en clase/14ServerSideRender/crudSSR/routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+const axios = require('axios');
+
+const httpAxios =  axios.create({
+  baseURL:'http://localhost:2500/v2/sextob/api/',
+})
+
+router.get('/', function(req, res, next) {
+  httpAxios.get(`productos`).then(respuesta=>{
+    console.log(respuesta.data.productos);
+    res.render('index', { productos: respuesta.data.productos });
+  })
+});
+
+module.exports = router;
